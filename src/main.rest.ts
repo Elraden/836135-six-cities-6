7c5ca44e4d6582d90243bf52e4ad08d46505b5d3
Elraden <morozovsa94@yandex.ru>
2024-03-05 14:18:04 +0300

    Добавлен DefaultCommentService

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -3,8 +3,9 @@ import 'reflect-metadata';
 import { RestApplication } from './rest/index.js';
 import { Component } from './types/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
-import { createUserContainer } from './shared/modules/user/user.container.js';
-import { createRentalOfferContainer } from './shared/modules/offer/offer.container.js';
+import { createUserContainer } from './shared/modules/user/index.js';
+import { createRentalOfferContainer } from './shared/modules/offer/index.js';
+import { createCommentContainer } from './shared/modules/comment/index.js';
 
 
 async function bootstrap() {
@@ -12,6 +13,7 @@ async function bootstrap() {
     createRestApplicationContainer(),
     createUserContainer(),
     createRentalOfferContainer(),
+    createCommentContainer(),
   );
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
